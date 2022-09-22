60293df26793446bc5c8611fbf0b31ce6b444877
Willichi <107594197+Willichi@users.noreply.github.com>
2022-09-22 21:40:07 +0400

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if__name__=="__main__":
-     from add_0 import add
+if _name_=="_main_":
+from add_0 import add
      a=1
      b=2
      print("{:d} + {:d} = {:d}".format(a,b,add(a, b)))
