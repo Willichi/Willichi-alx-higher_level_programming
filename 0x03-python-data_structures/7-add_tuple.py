41f19f5f09847772c931acd86cb3c771826223e6
Willichi <107594197+Willichi@users.noreply.github.com>
2022-09-24 12:59:59 +0400

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,15 +3,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
     """
     A function that adds 2 tuples.
     """
-    if len(tuple_a) < 2:
+    if len(tuple_a) < 2 or len(tuple_a) > 2:
         tuple_a = 0, 0
-    elif len(tuple_a) > 2:
-       tuple_a = 0, 0
+
+
     else:
         tuple_a = tuple_a[0], 0
-    if len(tuple_b) < 2:
-        tuple_b = 0, 0
-    elif len(tuple_b) > 2:
+    if len(tuple_b) < 2 or len(tuple_b) > 2:
+
+  
         tuple_b = 0, 0
     else:
        tuple_b = tuple_b[0], 0
