5c58977ba9288b5eab10cd84bb3aaac1e9659e9b
Willichi <onochiewilliam98@gmail.com>
2022-09-25 10:31:51 +0000

    task 8

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """
+    a function that finds the biggest integer of a list.
+    """
+    for i in range len(my_list) -1:
+    if my_list == {}:
+       return None
+     max_value = max (my_list)(i)
+       print("max_value: {} ".format(my_list(i)))
