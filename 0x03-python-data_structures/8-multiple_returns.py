2afa0f1fb8c52ec01eb7ac0df6cbc55ac43d5cf4
Willichi <onochiewilliam98@gmail.com>
2022-09-26 05:30:31 +0000

    task all

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    """
-    a function that returns a tuple with the length of a string and its first character.
-    """
-     for i in len(sentence) - 1:
-        if len(sentence) == {}:
-            first_character = sentence[i]
-               length = len(sentence)
-                first_character == None
-   
-         
-      print("length : {:d}, first_character: ".format(length, first_character)
+    if not sentence:
+        return (0, None)
+
+    return (len(sentence), sentence[0])
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    """
-    a function that finds the biggest integer of a list.
-    """
-    for i in range len(my_list) - 1:
-        if my_list == []:
-           return None
-              max_value = max_integer(my_list[i])
-         print("max_value: {:d} ".format(my_list[i]))
+    if not my_list:
+        return None
+
+    max_n = min(my_list)
+    for i in my_list:
+        if i > max_n:
+            max_n = i
+
+    return max_n
